fbe004663208bed2aa95ebfd55df9e50b3f48bc3
Oghenero Phoebe Sowho <phoebeoke3@gmail.com>
2024-09-04 18:51:29 +0100

    modified user.py

---
+++ models/user.py
@@ -0,0 +1,9 @@
+'''This is the user model'''
+
+class User:
+    '''This is the user class'''
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
