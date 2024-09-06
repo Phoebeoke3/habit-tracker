bf151afaf7f3206b75ad286ffc89fa7e580dad3c
Oghenero Phoebe Sowho <phoebeoke3@gmail.com>
2024-09-06 18:11:00 +0100

    updated user model

--- models/user.py
+++ models/user.py
@@ -7,3 +7,11 @@ class User:
         self.username = username
         self.email = email
         self.password = password
+
+    def __str__(self):
+        ''' A fuction that returns the string representation of an object'''
+        return f'''user 1
+-----------------------------------
+username:\t{self.username}
+email:\t{self.email}
+'''
